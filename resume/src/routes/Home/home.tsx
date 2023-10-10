007f582e54d9d2e8ca5758977786bd35c7344f05
Sedric <sedricgreen6@gmail.com>
2023-10-10 22:42:32 +0300

    create one-step-letter function

--- resume/src/routes/Home/home.tsx
+++ resume/src/routes/Home/home.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import Counter from '../../components/partials/Home/Counter';
-// import Quote from '../../components/shareds/Quote';
+import React, { useEffect, useState } from 'react';
+import aba from '../../assets/aba';
 
-const Home = () => (
-  <div className="home">
-    <Counter />
-    {/* <Quote /> */}
-  </div>
-);
+const Home = () => {
+  const [subtitle, setSubtitle] = useState(' ');
+  const text = aba[0];
+  let currentIndex = 0;
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setSubtitle((prev) => prev + text[currentIndex]);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      currentIndex += 1;
+      if (currentIndex === text.length - 1) {
+        clearInterval(id);
+      }
+    }, 30);
+    return () => clearInterval(id);
+  }, []);
+  return (
+    <div>
+      <div className="photo"> </div>
+      <div className="home">{subtitle}</div>
+    </div>
+  );
+};
 
 export default Home;
